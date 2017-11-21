3eff2677e8923e85f2c8545ab777009c12727ef5
fatov_di <fat-ne@yandex.ru>
2017-11-21 17:06:36 +0300

    lesson16

---
+++ src/Patterns/Pizza.java
@@ -0,0 +1,61 @@
+package Patterns;
+
+public class Pizza {
+    private boolean thick;
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "thick=" + thick +
+                ", sausege=" + sausege +
+                ", becon=" + becon +
+                ", cheese=" + cheese +
+                '}';
+    }
+
+    private int sausege;
+    private int becon;
+    private int cheese;
+
+    public Pizza(Builder builder) {
+        this.thick = builder.thick;
+        this.becon = builder.becon;
+        this.sausege = builder.sausege;
+        this.cheese = builder.cheese;
+    }
+
+    public static Builder builder1(boolean thick) {
+        return new Builder(thick);
+    }
+
+    public static class Builder{
+        private final boolean thick;
+        private int sausege = 0;
+        private int becon = 0;
+        private int cheese = 0;
+
+        public Builder(boolean thick) {
+            this.thick = thick;
+        }
+        
+
+        public Builder addSauseg(int sausege){
+            this.sausege = sausege;
+            return this;
+        }
+
+        public Builder addBecon(int becon){
+            this.becon = becon;
+            return this;
+        }
+
+        public Builder addCheese(int cheese){
+            this.cheese = cheese;
+            return this;
+        }
+
+        public Pizza build(){
+            return new Pizza(this);
+        }
+    }
+}
--- src/Patterns/Task16.java
+++ src/Patterns/Task16.java
@@ -3,6 +3,7 @@ package Patterns;
 public class Task16 {
     public static void main(String[] args) {
 
+        // 1
         Factory jp = Factory.getFactory("JP");
         Factory de = Factory.getFactory("DE");
         Factory ru = Factory.getFactory("RU");
@@ -14,6 +15,12 @@ public class Task16 {
         System.out.println("tayota: " + tayota.drive(100, 3));
         System.out.println("bmw: " + bmw.drive(100, 3));
         System.out.println("uaz: " + uaz.drive(100, 3));
+
+        // 2
+        Pizza pizza = new Pizza.Builder(true).addBecon(2).addCheese(3).build();
+        Pizza pizza1 = Pizza.builder1(true).addBecon(2).addSauseg(8).build();
+        System.out.println(pizza);
+        System.out.println(pizza1);
 
 
     }
