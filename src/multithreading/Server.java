2d5930d3403f6f1ec829f9f1a6fa4f42493fa192
fatov_di <fat-ne@yandex.ru>
2017-11-27 16:57:03 +0300

    lesson19

--- src/multithreading/Server.java
+++ src/multithreading/Server.java
@@ -1,6 +1,6 @@
 package multithreading;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -23,6 +23,8 @@ public class Server {
 
     private class Reader implements Runnable{
         private final Socket socket;
+        private String fileName;
+        private long sizeFile;
 
         private Reader(Socket socket) {
             this.socket = socket;
@@ -31,6 +33,23 @@ public class Server {
 
         @Override
         public void run() {
+
+            try(DataInputStream in = new DataInputStream(socket.getInputStream())) {
+
+                int len;
+                byte[] buffer = new byte[1024];
+                fileName = in.readUTF(); // имя файла
+                sizeFile = in.readLong(); // размер файла возможно будет использоваться для определения, хватит ли места или нет...
+
+                FileOutputStream fileOutputStream = new FileOutputStream(new File("D:\\Java\\Лекии_Задания" + fileName));
+
+                while ((len = in.read(buffer)) > 0){
+                    fileOutputStream.write(buffer); // Если не проканает, попробовать вот так: fileOutputStream.write(buffer, 0, len);
+                }
+
+            } catch (IOException e) {
+                System.out.println("Ошибка чтения файла!" + e.getMessage());
+            }
 
         }
     }
