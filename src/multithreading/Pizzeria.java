b96a72435ac00ed412ad4da24d7309b6f3bdbef8
fatov_di <fat-ne@yandex.ru>
2017-11-29 15:09:52 +0300

    lesson20

---
+++ src/multithreading/Pizzeria.java
@@ -0,0 +1,33 @@
+package multithreading;
+
+//import Lessons7andAbove.ArrayList;
+import Lessons7andAbove.List;
+import Patterns.Pizza;
+
+import java.util.ArrayList;
+
+public class Pizzeria {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+    private static class Order{
+//        private final List<Pizza> order;
+//        enum position {INWAITER, INCOOKER}
+//
+//        public Order(Pizza[] pizza) {
+//            order = new ArrayList<>();
+//            order.
+//        }
+    }
+
+    public void addOrder(Pizza... pizza){
+
+//        Order order = new Order(pizza);
+    }
+}
